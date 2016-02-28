ec571755009e1baee6f7f1f548b1cadf6935c6a0
snekkje <jkgulliksen@gmail.com>
2016-02-28 16:55:56 +1300

    first commit

---
+++ code.R
@@ -0,0 +1,8 @@
+#temp <- tempfile()
+#download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2", temp, mode = "wb")
+#StormData <- read.csv(temp)
+#unlink(temp)
+
+head(StormData)
+
+#use select dplyr
